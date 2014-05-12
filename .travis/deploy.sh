0707d33925f831c844cdea428047a62859ae3df2
Daniel Reverri <reverri@gmail.com>
2014-05-12 18:01:11 -0400

    Build docs from pull requests

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH != "master" ]; then
+if [ $TRAVIS_PULL_REQUEST != "false" ]; then
+	export BUILD_ID="$TRAVIS_BUILD_NUMBER-pr-$TRAVIS_PULL_REQUEST"
+elif [ $TRAVIS_BRANCH == "master" ]; then
+	export BUILD_ID=$TRAVIS_BUILD_NUMBER
+else
     echo "Skipping deploy of non-master branch..."
     exit 0
 fi
+
+mv build "apidocs-$BUILD_ID"
+tar -cjvf "apidocs-$BUILD_ID.tar.bz2" "apidocs-$BUILD_ID"
 
 cat > "s3cfg" << EOF
 [default]
@@ -11,4 +18,4 @@ access_key=$AWS_ACCESS_KEY
 secret_key=$AWS_SECRET_KEY
 EOF
 
-s3cmd -c s3cfg put apidocs-$TRAVIS_BUILD_NUMBER.tar.bz2 s3://io.orchestrate.deploy/
+s3cmd -c s3cfg put "apidocs-$BUILD_ID.tar.bz2" s3://io.orchestrate.deploy/

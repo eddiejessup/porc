48bc049051bf03211416883589da490020fff373
Maximiline Thayer <garbados@gmail.com>
2014-05-30 13:32:46 -0700

    drop 3.2 support

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(name='porc',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.2',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4'
       ],

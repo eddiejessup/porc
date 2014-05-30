1e3e07d3d0012d8c5ea6bb4b7a452203dfa0b2ab
Maximiline Thayer <garbados@gmail.com>
2014-05-30 10:54:17 -0700

    python 3 support :O

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setup(name='porc',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
-          # TODO Python 3 support
-          # 'Programming Language :: Python :: 3',
-          # 'Programming Language :: Python :: 3.2',
-          # 'Programming Language :: Python :: 3.3'
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4'
       ],
       )

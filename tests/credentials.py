811841ec4fde42738a1b638f01f127ee73a79615
Maximiline Thayer <deus@Maximilines-MacBook-Pro.local>
2014-05-29 18:26:07 -0700

    now set credentials via env var

--- tests/credentials.py
+++ tests/credentials.py
@@ -1,2 +1,4 @@
-# N.B.: This API key has been revoked.
-API_KEY = '5104351e-8167-4ff1-b441-3321f2771ece'
+import os
+
+# N.B.: the default API key has been revoked. To make tests work, set your own!
+API_KEY = os.environ.get('ORCHESTRATE_API_KEY', '5104351e-8167-4ff1-b441-3321f2771ece')

2fbf1548543394ea6c03512dd7714bd0068dd94c
Maximiline Thayer <garbados@gmail.com>
2014-06-01 00:20:24 -0700

    update credentials

--- tests/credentials.py
+++ tests/credentials.py
@@ -1,4 +1,4 @@
 import os
 
 # N.B.: the default API key has been revoked. To make tests work, set your own!
-API_KEY = os.environ.get('ORCHESTRATE_API_KEY', '5104351e-8167-4ff1-b441-3321f2771ece')
+API_KEY = os.environ.get('ORCHESTRATE_API_KEY', '42833fcb-e1cb-429b-ad02-95c263abf3a4')

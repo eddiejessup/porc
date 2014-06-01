65c313ddba38cdf82a6f23655f847467ae6957df
Maximiline Thayer <garbados@gmail.com>
2014-06-01 01:21:10 -0700

    [skip ci] version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='porc',
-      version='0.1.0',
+      version='0.2.0',
       description='Asynchronous Orchestrate.io Interface',
       author='Max Thayer',
       author_email='max@orchestrate.io',

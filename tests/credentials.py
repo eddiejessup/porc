c2665e2c0b1b4df2e0f4c439506f0fa50e5fca08
Maximiline Thayer <garbados@gmail.com>
2014-06-01 00:25:13 -0700

    try this on for size

--- tests/credentials.py
+++ tests/credentials.py
@@ -1,4 +1,4 @@
 import os
 
 # N.B.: the default API key has been revoked. To make tests work, set your own!
-API_KEY = os.environ.get('ORCHESTRATE_API_KEY', '42833fcb-e1cb-429b-ad02-95c263abf3a4')
+API_KEY = os.environ.get('ORCHESTRATE_API_KEY', '0117c3f4-e8c8-4a6d-97c1-29004e4d5531')

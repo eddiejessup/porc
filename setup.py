d2990cf2561c65ebcd313e12c905d0824a9bcdcf
Maximiline Thayer <garbados@gmail.com>
2014-07-08 08:18:55 -0700

    updated dependencies

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setup(name='porc',
       license='MIT',
       install_requires=[
           'requests-futures==0.9.4',
-          'vcrpy==1.0.2'
+          'vcrpy==1.0.2',
+          'lucene-querybuilder==0.2'
       ],
       test_suite="tests",
       classifiers=[
